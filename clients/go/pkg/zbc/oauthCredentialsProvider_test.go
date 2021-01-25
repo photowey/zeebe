dd5804cdef6e6a57e8cc936e9e6146cee645ad82
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2021-01-25 16:33:37 +0700

    feat(clients/go): Apply golangci-lint fixes

--- clients/go/pkg/zbc/oauthCredentialsProvider_test.go
+++ clients/go/pkg/zbc/oauthCredentialsProvider_test.go
@@ -758,7 +758,7 @@ func (s *oauthCredsProviderTestSuite) TestJobActivateRetry() {
 	// then
 	s.NoError(err)
 	s.GreaterOrEqual(1, credsProvider.(*custom).shouldRetryCalls)
-	s.NotEmpty(jobs.Jobs)
+	s.NotEmpty(jobs)
 }
 
 type custom struct {
