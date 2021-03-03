fef2d5c7071500c7535570f517fffd0c7a9279d4
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2021-03-03 15:56:31 +0100

    chore(journal): udpate serializer doc

--- journal/src/main/java/io/zeebe/journal/file/record/JournalRecordSerializer.java
+++ journal/src/main/java/io/zeebe/journal/file/record/JournalRecordSerializer.java
@@ -26,6 +26,7 @@ public interface JournalRecordSerializer {
    *
    * @param record to write
    * @param buffer to which the record will be written
+   * @param offset the offset in the buffer at which the data will be written
    * @return the number of bytes that were written to the buffer
    */
   int writeData(RecordData record, MutableDirectBuffer buffer, int offset);
@@ -35,6 +36,7 @@ public interface JournalRecordSerializer {
    *
    * @param metadata to write
    * @param buffer to which the metadata will be written
+   * @param offset the offset in the buffer at which the metadata will be written
    * @return the number of bytes that were written to the buffer
    */
   int writeMetadata(RecordMetadata metadata, MutableDirectBuffer buffer, int offset);
@@ -42,7 +44,7 @@ public interface JournalRecordSerializer {
   /**
    * Returns the number of bytes required to write a {@link RecordMetadata} to a buffer. The length
    * returned by this method must be equal to the length returned by {@link
-   * JournalRecordSerializer#writeMetadata(RecordMetadata, MutableDirectBuffer)}
+   * JournalRecordSerializer#writeMetadata(RecordMetadata, MutableDirectBuffer, int)}
    *
    * @return the expected length of a serialized metadata
    */
@@ -61,6 +63,7 @@ public interface JournalRecordSerializer {
    * buffer at this position.
    *
    * @param buffer to read
+   * @param offset the offset in the buffer at which the metadata will be read from
    * @return a journal record metadata that is read.
    */
   RecordMetadata readMetadata(DirectBuffer buffer, int offset);
@@ -70,14 +73,16 @@ public interface JournalRecordSerializer {
    * buffer at this position.
    *
    * @param buffer to read
+   * @param offset the offset in the buffer at which the data will be read from
    * @return a journal indexed record that is read.
    */
   RecordData readData(DirectBuffer buffer, int offset, int length);
 
   /**
-   * Returns the length of the serialized {@link RecordMetadata} at the buffer.
+   * Returns the length of the serialized {@link RecordMetadata} in the buffer.
    *
    * @param buffer to read
+   * @param offset the offset in the buffer at which the metadata will be read from
    * @return the length of the metadata
    */
   int getMetadataLength(DirectBuffer buffer, int offset);
